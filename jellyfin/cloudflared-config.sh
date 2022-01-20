8d4cec8fa2e4a9346e6f4e3f879d9656f4858afe
Trung Le <jackblk4@gmail.com>
2022-01-20 13:30:49 +0700

    fix: write cloudflared config

--- jellyfin/cloudflared-config.sh
+++ jellyfin/cloudflared-config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export TUNNELID=$(cloudflared tunnel list | grep 'homelab' | awk '{print $1;}')
-envsubst <template.yml >/etc/cloudflared/config.yml
+TUNNELID=$(cloudflared tunnel list | grep 'homelab' | awk '{print $1;}')
+envsubst <template.yml | sudo tee /etc/cloudflared/config.yml >/dev/null
